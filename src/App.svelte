a3f4c504beaafe4a3a1682a7794e1e3a40b709a6
Chris <chriscarton@live.fr>
2019-10-24 11:38:10 +0200

    Petite mise en page pas extraordinaire et ce sera tout

--- src/App.svelte
+++ src/App.svelte
@@ -21,43 +21,56 @@
 </header>
 
 <main>
-	<div id="Technos">
-		Html5 / Css3 
-		Sass
-		Javascript
-		Git / Github
-	</div>
-	<div id="modernDev">
-		Node JS 
-		Npm 
-		Gulp
-	</div>
-	<div id="Frameworks">
-		Wordpress 
-		CakePhp 
-		Symfony
-		React
-	</div>
-	<div id="Softwares">
-		Photoshop 
-		Illustrator
-		InDesign
+	<div class="part part-1">
+		<div id="Technos">
+			Html5 / Css3 <br>
+			Sass <br>
+			Javascript <br>
+			Git / Github <br>
+		</div>
+		<div id="modernDev">
+			Node JS <br>
+			Npm <br>
+			Gulp <br>
+		</div>
+		<div id="Frameworks">
+			Wordpress <br>
+			CakePhp <br>
+			Symfony <br>
+			React <br>
+		</div>
+		<div id="Softwares">
+			Photoshop <br>
+			Illustrator <br>
+			InDesign <br>
+		</div>
 	</div>
 
-	<div id="Experiences">
-		Les Sentinelles du web
-		L'imprimerie du centre 
-		Atout Pub
+	<div id="Experiences" class="part part-2">
+		Les Sentinelles du web <br> 
+		L'imprimerie du centre <br> 
+		Atout Pub <br>
 	</div>
-	<div id="Formation">
-		École Nationale Supérieure des Beaux Arts / 2011 - 2014
- 		Diplôme National d'Art Plastique (DNAP)
-		<hr>
-		AFPA Calais / 2008 - 2009
- 		Titre Professionnel d'Infographiste Metteur en page
+	<div id="Formation" class="part part-3">
+		École Nationale Supérieure des Beaux Arts / 2011 - 2014<br>
+ 		Diplôme National d'Art Plastique (DNAP)<br>
+		AFPA Calais / 2008 - 2009<br>
+ 		Titre Professionnel d'Infographiste Metteur en page<br>
 	</div>
 </main>
 
 <footer>
-
+	<p>Consultez aussi : </p>
+	<p>
+		Mon portfolio : 
+		<a href="https://github.com/chriscarton/portfolio" target="_blank">CLIQUEZ LÀ</a>
+	</p>
+	<p>
+		Mes créations graphiques : 
+		<a href="https://github.com/chriscarton/graphism" target="_blank">LÀ</a>
+	</p>
+	<p>
+		Mon github : 
+		<a href="https://github.com/chriscarton" target="_blank">ET ICI</a>
+	</p>
 </footer>
